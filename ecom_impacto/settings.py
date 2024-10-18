b70741e1cd7080d2bd02e46ba6534b8e68eac561
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2024-10-17 22:20:24 -0500

    se crearon modelos categoria, customer, producto, orden

--- ecom_impacto/settings.py
+++ ecom_impacto/settings.py
@@ -1,7 +1,5 @@
-
-
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -107,6 +105,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Directorio para img
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
