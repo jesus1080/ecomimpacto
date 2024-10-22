5aaa5e6311f6b33bb796f9328f75b10860c530ff
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2024-10-21 22:35:02 -0500

    add sale and sale price at product

--- store/models.py
+++ store/models.py
@@ -28,6 +28,8 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1) 
     description = models.CharField(max_length=250, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
