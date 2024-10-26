0fb4a84f2f831d2c8e838f447c85e5c579a7ac17
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2024-10-26 17:37:05 -0500

    feat: add login, logout and register

--- store/views.py
+++ store/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
+
 
 def home(request):
     productos = Product.objects.all()
@@ -8,3 +15,45 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("Estas authenticado correctamente"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Hay un error"))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+
+    
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("Saliste copn exito... Gracias!"))
+    return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Estas registrado correctamente"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Hay un problema con el registro"))
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form':form})
