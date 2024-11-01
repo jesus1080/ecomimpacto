a0d16fc9c8f9f108b1bc356419735a8df618acb4
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2024-10-31 23:10:48 -0500

    add product to cart

--- cart/cart.py
+++ cart/cart.py
@@ -12,3 +12,14 @@ class Cart():
         
         # estar seguro que cart es valido en todas las paginas del sitio
         self.cart = cart
+
+    def add(self, product):
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+
+
+        self.session.modified = True
--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 def cart_sumary(request):
     return render(request, "cart_sumary.html", {})
 
 def cart_add(request):
-    pass
+    # obtener el cart
+    cart = Cart(request)
+    # test post
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product)
+        response = JsonResponse({'Product Name': product.name})
+        return response
+
+
 
 def cart_delete(request):
     pass
