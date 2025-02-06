8bb36b5047800853271fdd13895e9ac72a03bdf2
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2025-02-06 10:19:14 -0500

    se agrega actualizacion de perfil de usuario

--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateUserForm
 from django import forms
 
 
@@ -72,6 +72,24 @@ def register_user(request):
             return redirect('register')
     else:
         return render(request, 'register.html', {'form':form})
+    
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+        if user_form.is_valid():
+            user_form.save()
+
+            login(request, current_user)
+            messages.success(request, "Usuario actualizado ")
+            return redirect('home')
+        return render(request, "update_user.html",{'user_form':user_form})
+    else:
+        messages.success(request, "Debes ingresar ")
+        return redirect('home')
+
+
     
 def category_summary(request):
     categories = Category.objects.all()
