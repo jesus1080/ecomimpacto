4d95955dc13e1ff7d058ee4bfa95c4a23485bff0
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2025-02-21 15:22:10 -0500

    se agrega proceso de generacion de orden

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from cart.cart import Cart
 from payment.forms import ShippingForm, PaymentForm
-from payment.models import ShippingAdrress
+from payment.models import ShippingAdrress, Order, OrderItem
 from django.contrib import messages
+from store.models import Product
 # Create your views here.
 def payment_success(request):
     return render(request, "payment/payment_success.html", {})
@@ -32,17 +33,103 @@ def billing_info(request):
         #cantidad de productos por unidad en el cart
         quantities = cart.get_quants
         totals = cart.cart_total()   
+        #crear session con la informacion de envio
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
         # Verificar si el usuario esta autenticado
-
         if request.user.is_authenticated:
             billing_form = PaymentForm()
             return render(request, "payment/billing_info.html", {'products': cart_products, 'quantities': quantities, 'totals': totals, 'shipping_info': request.POST, 'billing_form': billing_form} )
         else:
             billing_form = PaymentForm()
             return render(request, "payment/billing_info.html", {'products': cart_products, 'quantities': quantities, 'totals': totals, 'shipping_info': request.POST, 'billing_form': billing_form} )
-        shipping_form = request.POST
-        return render(request, "payment/billing_info.html", {'products': cart_products, 'quantities': quantities, 'totals': totals, 'shipping_form': shipping_form} )
+        
+        
     
     else:
         messages.success(request, ("Acceso Denegado"))
+        return redirect('home')
+    
+
+def process_order(request):
+    if request.POST: 
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        #cantidad de productos por unidad en el cart
+        quantities = cart.get_quants
+        totals = cart.cart_total()  
+        #obtenemos la informacion de factura para la ultima page
+        payment_form = PaymentForm(request.POST or None)
+        # obtener data de envio de la session
+        my_shipping = request.session.get('my_shipping')
+        # Gather=recopilar informacion de orden
+        full_name = my_shipping['shipping_full_name']
+        email = my_shipping['shipping_email']
+        #crear direccion de envio para la informacion de sesion
+        shipping_address = f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n"
+        amount_paid = totals
+
+        #crear una orden
+        if request.user.is_authenticated:
+            user = request.user
+
+            create_order = Order(user=user, full_name=full_name, email=email, shipping_address = shipping_address, amount_paid=amount_paid)
+            create_order.save()
+
+            #adicionar items a la orden
+            order_id = create_order.pk
+
+            for product in cart_products():
+                product_id = product.id
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+                #get cantidad
+                for key, value in quantities().items():
+                    if int(key) == product.id:
+                        # create order item
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user, quantity=value, price=price)
+                        create_order_item.save()
+            
+            #delete our car
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    del request.session[key]
+
+            messages.success(request, "Orden Enviada...")
+            return redirect('home')
+        else:
+            #no logeado
+            create_order = Order(full_name=full_name, email=email, shipping_address = shipping_address, amount_paid=amount_paid)
+            create_order.save()
+
+            #adicionar items a la orden
+            order_id = create_order.pk
+
+            for product in cart_products():
+                product_id = product.id
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+                #get cantidad
+                for key, value in quantities().items():
+                    if int(key) == product.id:
+                        # create order item
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value, price=price)
+                        create_order_item.save()
+
+            #delete our car
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    del request.session[key]
+
+            messages.success(request, "Orden Enviada...")
+            return redirect('home')
+
+      
+
+    else:
+        messages.success(request, "Acceso denegado")
         return redirect('home')
