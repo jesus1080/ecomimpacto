4563c18fe7ae671fdaebaefceae97c91a1ad670a
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2025-02-21 16:59:07 -0500

    se agrega fecha de envio y logica para guardado automatico

--- payment/admin.py
+++ payment/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from .models import ShippingAdrress, Order, OrderItem
+from django.contrib.auth.models import User
 
 # Register your models here.
 admin.site.register(ShippingAdrress)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+
+#crear on orden item inline
+class OrderItemInLine(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+
+#extend our order model
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    readonly_fields = ["date_ordered"]
+    inlines = [OrderItemInLine]
+
+#registrar
+admin.site.unregister(Order)
+admin.site.register(Order, OrderAdmin)
--- payment/models.py
+++ payment/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+import datetime
 # Create your models here.
 
 class ShippingAdrress(models.Model):
@@ -39,9 +41,21 @@ class Order(models.Model):
      shipping_address = models.TextField(max_length=1000)
      amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
      date_ordered = models.DateField(auto_now_add=True)
+     shipped = models.BooleanField(default=False)
+     date_shipped = models.DateTimeField(blank=True, null=True)
 
      def __str__(self):
          return f'order - {str(self.id)}'
+
+#auto adicion fecha de envio
+@receiver(pre_save, sender=Order)
+def set_shipped_date_on_update(sender, instance, **kwargs):
+    if instance.pk:
+        now = datetime.datetime.now()
+        obj = sender._default_manager.get(pk=instance.pk)
+        if instance.shipped and not obj.shipped:
+            instance.date_shipped = now
+
      
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
