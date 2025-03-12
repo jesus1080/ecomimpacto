0606df5566e043d6aa357dcc30246f774d4eca3f
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2025-03-12 08:27:49 -0500

    configuracion de archivos estaticos

--- ecom_impacto/settings.py
+++ ecom_impacto/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Directorio para templates
 
@@ -128,9 +128,9 @@ STATICFILES_DIRS = ['static/']
 # aqui whitenoise static stuff
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
